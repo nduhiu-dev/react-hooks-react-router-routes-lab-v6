ac0408276e385c75d819cf362515ee996cc58947
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-11-28 12:39:40 +0300

    Fixed errors and completed the lab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,14 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
-    );
-};
+    <div className="navbar">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
+      </nav>
+    </div>
+  );
+}
 
 export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Home Page</h1>
+
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <Link to={`/movie/${movie.id}`}>View Info</Link>
+        </div>
+      ))}
+    </div>
   );
-};
+}
 
 export default Home;
